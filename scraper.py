3170fd75508079035eba89ff5e72ddc243fc4856
Danny Khang Doan <doandk@circinus-43.ics.uci.edu>
2022-10-26 19:31:08 -0700

    scraper; get_next_links

--- scraper.py
+++ scraper.py
@@ -1,5 +1,6 @@
 import re
 from urllib.parse import urlparse
+from bs4 import BeautifulSoup
 
 def scraper(url, resp):
     links = extract_next_links(url, resp)
@@ -15,17 +16,46 @@ def extract_next_links(url, resp):
     #         resp.raw_response.url: the url, again
     #         resp.raw_response.content: the content of the page!
     # Return a list with the hyperlinks (as strings) scrapped from resp.raw_response.content
-    return list()
+    links = []
+    
+    if not resp.raw_response:
+        return links
+    soup = BeautifulSoup(resp.raw_response.content, 'html.parser')
+
+    for link in soup.find_all('a'): #find all anchor tags in the response content
+        foundLink = link.get('href')
+        linkWithNoFragment = re.sub(r'/#.+', '', str(foundLink))
+        links.append(linkWithNoFragment)
+        
+    return links
 
 def is_valid(url):
     # Decide whether to crawl this url or not. 
     # If you decide to crawl it, return True; otherwise return False.
     # There are already some conditions that return False.
+    if not url: return False
+
+    domains = [
+        '.ics.uci.edu/',
+        '.cs.uci.edu/',
+        '.informatics.uci.edu/',
+        '.stat.uci.edu/',
+        '.today.uci.edu/department/information_computer_sciences/'
+    ]
+
     try:
         parsed = urlparse(url)
+        found = False
+        for domain in domains:
+            if domain in url:
+                found = True
+                break
+        
         if parsed.scheme not in set(["http", "https"]):
             return False
-        return not re.match(
+        
+      
+        return found and not re.match(
             r".*\.(css|js|bmp|gif|jpe?g|ico"
             + r"|png|tiff?|mid|mp2|mp3|mp4"
             + r"|wav|avi|mov|mpeg|ram|m4v|mkv|ogg|ogv|pdf"
@@ -36,5 +66,6 @@ def is_valid(url):
             + r"|rm|smil|wmv|swf|wma|zip|rar|gz)$", parsed.path.lower())
 
     except TypeError:
+        print ("TypeError for ", url)
         print ("TypeError for ", parsed)
         raise
